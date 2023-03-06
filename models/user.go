0a78d10a7493da0eaae7aea4b54cbb48d5fbd41a
mpoweredo <mpow3r1337@gmail.com>
2023-03-06 06:03:52 +0100

    updated user model

--- models/user.go
+++ models/user.go
@@ -1,14 +1,14 @@
 package models
 
 import (
-	"time"
+	"database/sql"
 )
 
 type User struct {
-	Id        uint      `gorm:"primary_key" gorm:"autoIncrement" json:"id"`
-	Username  string    `json:"username" gorm:"type:varchar(100);not null" validate:"required,min=5,max=24"`
-	Email     string    `json:"email" gorm:"type:varchar(100);unique;not null" validate:"required,email,min=6,max=48"`
-	Password  string    `json:"password" gorm:"type:varchar(100);not null" validate:"required,min=5,max=24"`
-	UpdatedAt time.Time `json:"updatedAt"`
-	CreatedAt time.Time `json:"createdAt"`
+	Id        uint         `gorm:"primary_key" gorm:"autoIncrement" json:"id"`
+	Username  string       `json:"username" gorm:"type:varchar(100);not null" validate:"required,min=5,max=24"`
+	Email     string       `json:"email" gorm:"type:varchar(100);unique;not null" validate:"required,email,min=6,max=48"`
+	Password  string       `json:"password" gorm:"type:varchar(100);not null" validate:"required,min=5,max=24"`
+	UpdatedAt sql.NullTime `json:"updatedAt"`
+	CreatedAt sql.NullTime `json:"createdAt"`
 }
